9dd2b0b2826bf400b0d84ad87bc5db134cdcb0b8
Cheng Xie <cheng.k.xie@gmail.com>
2017-04-06 13:34:34 -0700

    Added atompotential

--- potentials.js
+++ potentials.js
@@ -15,7 +15,26 @@ function GravityZeroPotential(){
         return this.gravConst*pot;
     }
 }
-
+function AtomZeroPotential(){
+    this.atomConst = 1;
+    this.pushConst = 15.0;
+    this.staticPot = function(position){
+        var Ux = Math.max(0,Math.abs(position.x)-240)*100;
+        var Uz = Math.max(0,Math.abs(position.z)-240)*100;
+        return Ux+Uz; 
+    }
+    this.potential = function(balls, position){
+        var pot = this.staticPot(position);
+        for (var i = 0; i < balls.length; ++i){
+            // Calculate the distance
+            var dist = calcXZDist(balls[i].position, position) 
+            // Clamp the dist at some value
+            dist = Math.max(0.2, dist);
+            pot += -balls[i].mass/dist + this.pushConst*balls[i].mass/dist/dist;
+        }
+        return this.atomConst*pot;
+    }
+}
 // HELPER Functions
 function calcXZDist(position1, position2){
     var xdist = (position1.x-position2.x);
